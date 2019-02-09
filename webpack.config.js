862b665003364cb211a8189eb166a3d38da573c7
sadness_ojisan <yuta_ide@r.recruit.co.jp>
2019-02-09 17:10:41 +0900

    add: index.html

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const env = process.env.BUILD_MODE;
+
+module.exports = {
+  mode: env || "development",
+  entry: "./src/main.jsx",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "build.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".json", ".jsx"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    historyApiFallback: true
+  }
+};
