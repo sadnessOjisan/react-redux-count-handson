e0c79a220554d1a2c46c64db3dbf3e22e719194d
sadness_ojisan <yuta_ide@r.recruit.co.jp>
2019-02-13 12:25:57 +0900

    update: count reset

--- src/countModule.js
+++ src/countModule.js
@@ -3,24 +3,25 @@
 // action type
 const COUNT_UP = "COUNT_UP";
 const COUNT_DOWN = "COUNT_DOWN";
-
-const actionTypes = {
-  COUNT_UP,
-  COUNT_DOWN
-};
+const RESET = "RESET";
 
 // action cretor
 
 type TCountUpAction = { type: typeof COUNT_UP };
 type TCountDownAction = { type: typeof COUNT_DOWN };
-type TAction = TCountUpAction | TCountDownAction;
+type TResetAction = { type: typeof RESET };
+type TAction = TCountUpAction | TCountDownAction | TResetAction;
 
 export const countUp = (): TCountUpAction => {
   return { type: COUNT_UP };
 };
 
 export const countDown = (): TCountDownAction => {
-  return { type: COUNT_DOWN, payload: 2 };
+  return { type: COUNT_DOWN };
+};
+
+export const reset = (): TResetAction => {
+  return { type: RESET };
 };
 
 // state
@@ -39,6 +40,8 @@ const reducer = (state: TState = initialState, action: TAction) => {
       return { ...state, count: state.count + 1 };
     case COUNT_DOWN:
       return { ...state, count: state.count - 1 };
+    case RESET:
+      return initialState;
     default:
       return initialState; // これがないとundefinedがreturnされるよ https://qiita.com/kiita312/items/7fdce94912d6d9c801f8
   }
