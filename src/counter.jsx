d1112a3225dc090cfc01def4000c7f0c0559dda3
sadness_ojisan <sadness.ojisan@gmail.com>
2019-02-16 01:40:01 +0900

    update: class

--- src/counter.jsx
+++ src/counter.jsx
@@ -4,15 +4,20 @@ import { connect } from "react-redux";
 import { countUp, countDown, reset } from "./countModule";
 
 /**
- * This is MyClass.
- * @reactProps {!number} count - storeが管理しているcountのstate
+ * CounterのUIを作るクラスコンポーネント。
+ * Reactのクラスコンポーネントは必ず`render()`メソッドを持たなければならない。
+ * @reactProps {Number} count - storeが管理しているcountのstate
  * @reactProps {void=>void} countUp - countUp actionのdispatcher
  * @reactProps {void=>void} countDown - countDown actionのdispatcher
  * @reactProps {void=>void} reset - reset actionのdispatcher
  */
 class Counter extends React.Component {
   /**
-   * propsが更新されるたびに呼び出されるレンダリング関数
+   * stateやpropsを元にUIを返す関数.
+   * コンポーネントがmountされたとき、propsやstateが更新されたときに自動で呼ばれる。
+   * renderのライフサイクルについては [こちら(Understanding React — React 16.3 + Component life-cycle)](https://medium.com/@baphemot/understanding-react-react-16-3-component-life-cycle-23129bc7a705)を参照すると良いでしょう。
+   * またrenderの仕様は [こちら(React.Component#render)](https://reactjs.org/docs/react-component.html#render)を参照すると良いでしょう。
+   * @returns {void}
    */
   render() {
     const { count, countUp, countDown, reset } = this.props;
